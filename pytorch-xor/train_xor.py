3ba4ca487c32bc61366efeecdf36b37f1adf39e5
Clayton Morrison <claytonm@email.arizona.edu>
2020-10-26 12:12:18 -0700

    finished pbs example

--- pytorch-xor/train_xor.py
+++ pytorch-xor/train_xor.py
@@ -272,7 +272,7 @@ def main() -> None:
     save_results(output_root, loss_values, accuracy_values)
     write_to_log(log_file, 'Saving results DONE')
 
-    if args.save_model is not None:
+    if args.save_model:
         model_dst_file = os.path.join(output_root, 'xor_model.pt')
         write_to_log(log_file, f'Saving model to {model_dst_file}')
         torch.save(model.state_dict(), model_dst_file)
